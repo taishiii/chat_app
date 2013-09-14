76c571d6de7f20b30300abe6ca103623df00ae49
taishiii <taishi.hiraga@gmail.com>
2013-09-14 13:22:01 +0900

    syntax error debug

--- php/comet.php
+++ php/comet.php
@@ -24,27 +24,30 @@ function pushData($data) {
         $data = str_replace(array("\n", "\r"), '', $data) . '[' . date('c') . ']' . PHP_EOL;
         
         file_put_contents(DATA_FILE, $data, FILE_APPEND|LOCK_EX);
+    }
+    return getData();
 }
-return getData();
 
+if (isset($_GET['mode'])) {
+    //guide to mode
+    switch ($_GET['mode']) {
+    
+        //get data
+        case 'view':
+            $data = getData();
+            break;
 
-set($_GET['mode']) {
-//guide to mode
-    //get data
-    case 'view':
-        $data = getData();
-        break;
+        //update check
+        case 'check':
+            $data = getUpdatedData();
+            break;
 
-    //update check
-    case 'check':
-        $data = getUpdatedData();
-        break;
-
-    //save data
-    case 'add':
-        $data = pushData($_POST['data']);
-        break;
-}
+        //save data
+        case 'add':
+            $data = pushData($_POST['data']);
+            break;
+    }
 
 //output result
 echo nl2br(htmlspecialchars($data, ENT_QUOTES));
+}
